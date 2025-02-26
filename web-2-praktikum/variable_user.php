96dad8f3d9748fb78837bcffc0ab9681bb27ffc9
ulan1208 <ulandr232@gmail.com>
2025-02-26 13:54:31 +0700

    Add files via upload

---
+++ web-2-praktikum/variable_user.php
@@ -0,0 +1,10 @@
+<?php
+// mendefinisikan variabel
+$nama = 'ulan';
+$umur = '17';
+$berat = 60.60;
+
+echo 'Nama saya' . $nama;
+echo '<br/>umur :' . $umur . 'Tahun';
+echo '<br/>Berat' . $nama;
+?>
