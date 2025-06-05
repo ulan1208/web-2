60da70d4a531223ab472b68f64aa53756d57647d
ulan1208 <ulandr232@gmail.com>
2025-06-05 13:23:27 +0700

    praktikum11

--- room-booking/resources/views/livewire/unit-kerja/edit-unit-kerja.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container mx-auto">
-    <h1 class="text-2xl font-bold mb-4">Edit Ruang {{ $ruang->kode }}</h1>
-    @if ($unitKerjaId)
-        <form wire:submit.prevent="update">
-            <input type="text" wire:model="nama_unit_kerja" placeholder="Nama Unit Kerja">
-            <button type="submit">Update</button>
-        </form>
-    @endif
-</div>
